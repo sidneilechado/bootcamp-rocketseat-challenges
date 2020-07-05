95b6245637f9852c0588fb6a69bcacd2c1c8b407
sid <juniorlechado@gmail.com>
2020-07-05 15:25:06 -0300

    Solved
    Signed-off-by: sid <juniorlechado@gmail.com>

--- 07-primeiro-projeto-reactjs/src/pages/Dashboard/index.tsx
+++ 07-primeiro-projeto-reactjs/src/pages/Dashboard/index.tsx
@@ -23,19 +23,59 @@ interface Transaction {
   created_at: Date;
 }
 
+interface TransactionUnformated {
+  id: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: string;
+}
+
 interface Balance {
   income: string;
   outcome: string;
   total: string;
 }
 
+interface TransactionsResponseDTO {
+  balance: Balance;
+}
+
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const { data } = await api.get(`transactions`);
+
+      const balanceData: Balance = data.balance;
+
+      const balanceFormated: Balance = {
+        income: formatValue(Number(balanceData.income)),
+        outcome: formatValue(Number(balanceData.outcome)),
+        total: formatValue(Number(balanceData.total)),
+      };
+
+      const formattedTransactions: Transaction[] = data.transactions.map(
+        (transaction: TransactionUnformated) => {
+          const formattedValue = formatValue(Number(transaction.value));
+
+          const created_at = new Date(transaction.created_at); // eslint-disable-line
+          const formattedDate = new Intl.DateTimeFormat('pt-BR').format(created_at);
+
+          return {
+            ...transaction,
+            created_at, // eslint-disable-line
+            formattedDate,
+            formattedValue,
+          } as Transaction;
+        },
+      );
+
+      setBalance(balanceFormated);
+      setTransactions(formattedTransactions);
     }
 
     loadTransactions();
@@ -51,21 +91,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +121,18 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome'
+                      ? `- ${transaction.formattedValue}`
+                      : transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
--- 07-primeiro-projeto-reactjs/src/pages/Import/index.tsx
+++ 07-primeiro-projeto-reactjs/src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    const requestsFiles: Promise<void>[] = uploadedFiles.map(file => {
+      data.append('file', file.file);
+      return api.post('/transactions/import', data);
+    });
 
     try {
       // await api.post('/transactions/import', data);
+      await Promise.all(requestsFiles);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const formattedFiles = files.map<FileProps>(file => ({
+      file,
+      name: file.name,
+      readableSize: file.size.toString(),
+    }));
+
+    setUploadedFiles(formattedFiles);
   }
 
   return (
